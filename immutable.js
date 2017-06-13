2f4f4c564ec4030a02c2d54b63c809845f6b0bc6
Sam Olaogun <sam.olaogun@gmail.com>
2017-06-13 19:01:32 -0400

    Create immutable.js

--- immutable.js
+++ immutable.js
@@ -1,47 +1,42 @@
 /**
  * Immutable
  * 
- * A bare-bones immutable linked list module.
- * 
- * @author Sam Olaogun
+ * @author  Sam Olaogun
+ * @license MIT
  */
 'use strict';
 
 /**
- * Constructor function — transforms the primitive array.
- * 
- * @param {Object[]} store - Array that stores the transformed data.
- * @param {number} [position=0] - Position property, zero by default.
+ * @param {Object[]} store          Array that stores the transformed data.
+ * @param {number}   [position=0]   Position property, zero by default.
  */
-function Immutable(store, position) {
+function Immutable (store, position) {
     this.store = [];
-    this.position = position || 0;
-
+    this.position = position >>>= 0;
+    
     store.forEach(value => {
-        if (!this.includes(value)) {
-            const node = {
+        if (!this.includes(value))
+            this.store.push({
                 value: value,
-                next: null
-            }
-
-            let p;
-            if (p = this.peek()) p.next = node;
-            this.store.push(node);
-        }
+                next: ~this.next() ? null : this.next()
+            });
+        
+        this.position++;
     });
-
+  
+    this.position = position;
     Object.freeze(this.store);
-}
+};
 
 Immutable.prototype = {
-    includes: function(val) { return !this.store.every(i => i != val); },
-    peek: function() { return this.store[this.store.length - 1]; },
-    next: function() { return this.store[++this.position].value || -1; },
-    front: function() { this.position = 0; },
-    back: function() { this.position = this.store.length - 1; },
-    getElement: function() { return this.store[this.position]; },
-    moveTo: function(pos) { this.position = pos; },
-    length: function() { return this.length; }
-}
+    includes: val => !this.store.every(i => i !== val),
+    peek: () => this.store[this.store.length - 1],
+    next: () => this.store[++this.position].value || -1,
+    front: () => this.position = 0,
+    back: () => this.position = this.store.length - 1,
+    getElement: () => this.store[this.position],
+    moveTo: pos => this.position = pos,
+    length: () => this.length
+};
 
 module.exports = Immutable;
